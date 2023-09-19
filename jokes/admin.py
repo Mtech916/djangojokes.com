a2d68d496e3468e4d9f14b7a5588c5c3a9ba2d31
Mtech916 <shredtechfit@gmail.com>
2023-09-19 11:35:47 -0700

    Alter jokes model to use slugs

--- jokes/admin.py
+++ jokes/admin.py
@@ -10,6 +10,6 @@ class JokeAdmin(admin.ModelAdmin):
 
     def get_readonly_fields(self, request, obj=None):
         if obj:
-            return ("created", "updated")
+            return ("slug", "created", "updated")
 
         return ()
