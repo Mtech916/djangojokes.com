40215cbbb2dac92308c5b35fa0f3ad89a104160c
Mtech916 <shredtechfit@gmail.com>
2023-09-20 15:44:40 -0700

    Created joke categories field

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from .models import Joke
+from .models import Category, Joke
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ["category", "created", "updated"]
+
+    def get_readonly_field(self, request, obj=None):
+        if obj:
+            return ("slug", "created", "updated")
+        return ()
 
 
 @admin.register(Joke)
--- jokes/models.py
+++ jokes/models.py
@@ -4,9 +4,32 @@ from django.urls import reverse
 from common.utils.text import unique_slug
 
 
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True, null=False, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse("jokes:category", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.category
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
 class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     slug = models.SlugField(max_length=50, unique=True, null=False, editable=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
