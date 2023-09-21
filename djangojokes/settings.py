c7747750dc81eb35d9d44734be5afe28face8193
Mtech916 <shredtechfit@gmail.com>
2023-09-21 11:42:50 -0700

    Setup Allauth lib for user reg

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     # Third-party apps
     "crispy_forms",
     "crispy_bootstrap5",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
     # Local apps
     "common.apps.CommonConfig",
     "jobs.apps.JobsConfig",
@@ -50,6 +53,8 @@ INSTALLED_APPS = [
     "pages.apps.PagesConfig",
     "users.apps.UsersConfig",
 ]
+
+SITE_ID = 1
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
@@ -63,6 +68,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Third-party middleware
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "djangojokes.urls"
@@ -100,6 +107,22 @@ DATABASES = {
     }
 }
 
+# Email
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+DEFAULT_FROM_EMAIL = "agenthiggins@gmail.com"
+
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, even w/o 'allauth'
+    "django.contrib.auth.backends.ModelBackend",
+    # 'allauth'-specific auth methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -121,6 +144,21 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Authentication Settings
 AUTH_USER_MODEL = "users.CustomUser"
+
+# AUTHENTICATION SETTINGS
+AUTH_USER_MODEL = "users.CustomUser"
+LOGIN_URL = "account_login"
+LOGIN_REDIRECT_URL = "pages:homepage"
+
+# django-allauth settings
+ACCOUNT_AUTHENTICATION_METHOD = "email"  # Default: 'username'
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1  # Default: 3
+ACCOUNT_EMAIL_REQUIRED = True  # Default: False
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"  # Default: 'optional'
+ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 5  # Default: 5
+ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 300  # Default 300
+ACCOUNT_LOGOUT_REDIRECT_URL = "account_login"  # Default: '/'
+ACCOUNT_USERNAME_REQUIRED = False  # Default: True
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
