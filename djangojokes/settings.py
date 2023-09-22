b19a056371548dddb954864689f7448985f4727d
Mtech916 <shredtechfit@gmail.com>
2023-09-21 17:39:52 -0700

    Added email txt/html templates, new Signup form

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -160,6 +160,8 @@ ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 300  # Default 300
 ACCOUNT_LOGOUT_REDIRECT_URL = "account_login"  # Default: '/'
 ACCOUNT_USERNAME_REQUIRED = False  # Default: True
 
+ACCOUNT_SIGNUP_FORM_CLASS = "users.forms.SignupForm"
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
