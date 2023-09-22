786d63f4a18b07aedc7bf457b5cccd26f9278fae
Mtech916 <shredtechfit@gmail.com>
2023-09-22 13:49:26 -0700

    Added success mixins for jokes

--- jokes/views.py
+++ jokes/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView,
@@ -12,9 +13,10 @@ from .models import Joke
 from .forms import JokeForm
 
 
-class JokeCreateView(LoginRequiredMixin, CreateView):
+class JokeCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Joke
     form_class = JokeForm
+    success_message = "Joke created."
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -38,9 +40,10 @@ class JokeListView(ListView):
     model = Joke
 
 
-class JokeUpdateView(UserPassesTestMixin, UpdateView):
+class JokeUpdateView(SuccessMessageMixin, UserPassesTestMixin, UpdateView):
     model = Joke
     form_class = JokeForm
+    success_message = "Joke Updated."
 
     def test_func(self):
         obj = self.get_object()
