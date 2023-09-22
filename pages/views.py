a4637380b31c172a77807c65d12a8d6a4cdb3b94
Mtech916 <shredtechfit@gmail.com>
2023-09-22 13:09:19 -0700

    Added django.messages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.views.generic import TemplateView
 
 
@@ -7,3 +8,11 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "pages/about-us.html"
+
+    # def get(self, request, *args, **kwargs):
+    #     messages.debug(request, "Debug message.")
+    #     messages.info(request, "Info message.")
+    #     messages.success(request, "Success message.")
+    #     messages.warning(request, "Warning message.")
+    #     messages.error(request, "Error message.")
+    #     return super().get(request, args, kwargs)
