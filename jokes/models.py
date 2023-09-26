01e392babe43637dafa757930ea5422f0cb4349b
Mtech916 <shredtechfit@gmail.com>
2023-09-26 11:36:22 -0700

    Implement search, set aggregation

--- jokes/models.py
+++ jokes/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Avg
 from django.urls import reverse
 
 from common.utils.text import unique_slug
@@ -53,6 +54,16 @@ class Joke(models.Model):
     @property
     def num_dislikes(self):
         return self.jokevotes.filter(vote=-1).count()
+
+    @property
+    def rating(self):
+        if self.num_votes == 0:  # No jokes, so rating is 0
+            return 0
+
+        r = JokeVote.objects.filter(joke=self).aggregate(average=Avg("vote"))
+
+        # Reutn the rounded rating.
+        return round(5 + (r["average"] * 5), 2)
 
     def get_absolute_url(self):
         return reverse("jokes:detail", args=[str(self.slug)])
--- jokes/views.py
+++ jokes/views.py
@@ -2,6 +2,7 @@ import json
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import Q
 from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -102,6 +103,10 @@ class JokeListView(ListView):
     def get_queryset(self):
         ordering = self.get_ordering()
         qs = Joke.objects.all()
+
+        if "q" in self.request.GET:  # FIlter by search query
+            q = self.request.GET.get("q")
+            qs = qs.filter(Q(question__icontains=q) | Q(answer__icontains=q))
 
         if "slug" in self.kwargs:  # Filter by category or tag
             slug = self.kwargs["slug"]
